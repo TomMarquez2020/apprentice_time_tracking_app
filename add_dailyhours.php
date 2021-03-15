bdb904a43362281274ab058c45bf84bfa9060d7f
TomMarquez2020 <Tom.Marquez2020@gmail.com>
2021-03-14 23:05:58 -0700

    added some boiler plate htmle to add_dailyhours.php as well as some notes

---
+++ add_dailyhours.php
@@ -0,0 +1,75 @@
+<?php require_once('connect/conn.php'); ?>
+<?php
+// php here will need to pull the following info:
+//          1. Supervisorname of apprentice id
+//          2. All work processes of apprenctice
+//              a. Order by process character
+//          3. all hour work and process character of the passed in date for apprentice if any
+
+//  roughly this:
+
+$data     = $_GET['data'];
+// split data into 3 variables
+list($id, $month, $year) = explode("_", $data);
+$data_array   = array();
+
+$query = "SELECT 
+            supervisor.fname, supervisor.lname
+
+            FROM personoccupationstbl p
+            JOIN occupationworkprocessestbl ow ON ow.occfk = p.occpersoccfk 
+            LEFT JOIN apprenticeoccupationprogresstbl aop ON aop.poaopfk = p.poid 
+                                    AND aop.owpfk = ow.owpid
+            JOIN apprentsuperstbl aps ON aps.persappsupfk = p.perspersoccfk
+            JOIN supervisorstbl s ON s.supervisorid = aps.supappsupfk
+            JOIN personoccupationstbl supocc ON supocc.perspersoccfk = s.perssupfk
+            JOIN personstbl supervisor ON supervisor.personid = supocc.perspersoccfk
+            WHERE p.perspersoccfk = 1
+                ";
+
+// Question here is do we want to get the all of the data for this page with one SQL call, or break it out?
+
+$rs = mysqli_query($con, $query);
+while ($sql_data = mysqli_fetch_assoc($rs)) {
+    $data_array[] = $sql_data;
+}
+$sql_data = json_encode($data_array);
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Add Daily Hours</title>
+    <link rel="stylesheet" href="css/styles.css">
+</head>
+
+<body>
+    <main id="ad_dailyhours">
+        <section id="ad_left">
+            <h2 id="ad_date">Date Goes here</h2>
+            <!--    Table here for any work done followed by 2 text fields (1 for adding hours and 1 for adding process) 
+                    The second text field could be a drop down of processes
+            -->
+        </section>
+        <section id="ad_right">
+            <h2>Work Processes</h2>
+            <!--    Another table here for character process and process name -->
+
+        </section>
+    </main>
+
+
+    <script src="js/jquery.js"></script>
+    <script src="js/add_dailyhours_script.js"></script>
+</body>
+
+</html>
+
+<?php
+mysqli_close($con);
+?>
--- month_calendar.php
+++ month_calendar.php
@@ -3,72 +3,72 @@
 <?php require_once('connect/conn.php'); ?>
 <?php
 
-    $data     = $_GET['data'];
-    // split data into 3 variables
-    list($id, $month, $year) = explode("_", $data);
-    $data_array   = array();
+$data     = $_GET['data'];
+// split data into 3 variables
+list($id, $month, $year) = explode("_", $data);
+$data_array   = array();
 
-    $query = "SELECT 
-                    DAY(aop.date) AS Day,
-                    SUM(aop.hours) as DailySum
-                    FROM personoccupationstbl p
-                    join apprenticeoccupationprogresstbl aop ON aop.poaopfk = p.poid
-                    where p.perspersoccfk = $id
-                    AND YEAR(aop.date) = $year 
-                    AND MONTHNAME(aop.date) = '$month'
-                    GROUP BY aop.date
-                ";
-        $rs = mysqli_query($con, $query);
-        while($sql_data = mysqli_fetch_assoc($rs)){
-            $data_array[] = $sql_data;
-        }
-        $sql_data = json_encode($data_array);
+$query = "SELECT 
+                        DAY(aop.date) AS Day,
+                        SUM(aop.hours) as DailySum
+                        FROM personoccupationstbl p
+                        join apprenticeoccupationprogresstbl aop ON aop.poaopfk = p.poid
+                        where p.perspersoccfk = $id
+                        AND YEAR(aop.date) = $year 
+                        AND MONTHNAME(aop.date) = '$month'
+                        GROUP BY aop.date
+                    ";
+$rs = mysqli_query($con, $query);
+while ($sql_data = mysqli_fetch_assoc($rs)) {
+    $data_array[] = $sql_data;
+}
+$sql_data = json_encode($data_array);
 
 ?>
 
 <!DOCTYPE html>
 <html lang="en">
 
-    <head>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <link rel="stylesheet" href="css/styles.css">
-        <title>Month Calendar</title>
-        <style>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/styles.css">
+    <title>Month Calendar</title>
+    <style>
 
-        </style>
-    </head>
+    </style>
+</head>
 
-    <body>      
-        <div id="mc_main">
-            <section>
-                <h2 id="mc_data"><?php echo $month . ' ' . $year ?></h2>
+<body>
+    <main id="mc_main">
+        <section>
+            <h2 id="mc_data"><?php echo $month . ' ' . $year ?></h2>
+        </section>
+
+        <form type="post">
+            <input type="hidden" id="data" name="data" value='<?php echo $sql_data ?>'>
+            <input type="hidden" id="month" name="month" value="<?php echo $month ?>">
+            <input type="hidden" id="year" name="year" value="<?php echo $year ?>">
+            <input type="hidden" id="id" name="id" value="<?php echo $id ?>">
+
+            <section id="mc_calendar_block">
+                <table id="mc_table">
+
+                </table>
             </section>
-            
-            <form type="post">
-                <input type="hidden" id="data" name="data" value='<?php echo $sql_data ?>'>
-                <input type="hidden" id="month" name="month" value="<?php echo $month ?>">
-                <input type="hidden" id="year" name="year" value="<?php echo $year ?>">
-                <input type="hidden" id="id" name="id" value="<?php echo $id ?>">
 
-                <section id="mc_calendar_block">
-                    <table id="mc_table">
+        </form>
 
-                    </table>
-                </section>
+        <input type="submit" button="submit">
 
-            </form>
+    </main>
 
-            <input type="submit" button="submit">
-
-        </div>
-
-        <script src="js/jquery.js"></script>
-        <script src="js/month_calendar_script.js"></script>
-    </body>
+    <script src="js/jquery.js"></script>
+    <script src="js/month_calendar_script.js"></script>
+</body>
 
 </html>
 
 <?php
-    mysqli_close($con);
+mysqli_close($con);
 ?>
